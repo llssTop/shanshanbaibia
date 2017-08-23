e7fadd4926dba0cfac6d87202ccf95eee46f1521
baijingqi521 <2491029161@qq.com>
2017-08-23 09:45:48 +0800

    tonghuo

--- application/admin/controller/Auth.php
+++ application/admin/controller/Auth.php
@@ -1,33 +1,64 @@
 <?php
-namespace app\admin\controller;
+namespace app\admin\Controller;
+use app\admin\model\Auth;
 use think\Controller;
-use app\admin\model\User;
-class Auth extends Controller {
+use think\Validate;
+class Auth extends Controller 
+{
 	protected $is_login = [''];
-	public function _initialize() {
-		if (!$this->checklogin() && $this->is_login[0] == '*') {
-			$this->error('您还没有登录，请先登录', url('admin/auth/login'));
+	public function _initialize()
+	{
+		if(!$this->checklogin() && $this->is_login[0] == '*')
+		{
+			$this->error('您还未登录，请先登录',url('admin/auth/login'));
 		}
 	}
-	public function checklogin() {
+	public function checklogin()
+	{
 		return session('?admin');
-		//判断session里面存储是否有值
 	}
-	public function login() {
+	public function login()
+	{
 		return $this->fetch();
 	}
-	public function dologin() {
-		// 对登录进行处理
-		$info = User::where(['nickname' => input('name')])->find();
-		if ($info) {
-			session('admin', $info->toArray());
-			$this->success('成功', url('admin/admin/index'));
-		} else {
-			$this->error('您还没有登录，请先登录', url('admin/auth/login'));
+	public function dologin(Auth $auth)
+	{
+		$validate = new Validate([
+			'captcha|验证码'=>'require|captcha'
+			]);
+		$data = [
+			'captcha' => $this->request->param('code'),
+		];
+		if(!$validate->check($data))
+		{
+			$this->error('验证码错误');
+		}
+
+		$info = $auth->checkusernameModle();
+		if(!$info){
+			$this->error('用户不存在');
+			exit;
+		}
+		$userinfo = $auth->checkpwdModel($info);
+		if($userinfo){
+			session('admin', $info);
+			$this->success('登录成功','admin/index/index');
+		}else{
+			$this->error('密码错误');
 		}
 	}
-	public function loginout() {
+	/*public function checkusername(Auth $auth){
+		$name = $_POST['name'];
+		$re = $auth->checkusernameModle($name);
+		if($re){
+			echo json_encode('存在');
+		}else{
+			echo json_encode('不存在');
+		}
+	}*/
+	public function loginout()
+	{
 		session(null);
-		$this->error('退出登录成功', url('admin/auth/login'));
+		$this->error('您还未登录，请先登录',url('admin/auth/login'));
 	}
 }
